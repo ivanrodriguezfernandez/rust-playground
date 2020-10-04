61c8257f01abb73c1a56b0599f4a8522f7caaabd
Iván Rodríguez <ivanrodriguezfernandez@gmail.com>
2020-10-04 18:55:04 +0200

    Update clock exercise

--- Exercism/rust/clock/src/lib.rs
+++ Exercism/rust/clock/src/lib.rs
@@ -11,18 +11,27 @@ impl Clock {
         }
     }
     pub fn to_string(&self) -> String {
-        format!("{}:{}", Clock::add_zero(self.h), Clock::add_zero(self.m))
+        format!("{}:{}", Clock::hour_formater(self.h), Clock::hour_formater(self.m))
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
         unimplemented!("Add {} minutes to existing Clock time", minutes);
     }
 
-    pub fn add_zero(num: i32) -> String {
-        if num.to_string().capacity() == 1 {
-            format!("0{}", num.to_string())
-        } else {
-            num.to_string()
+    pub fn hour_formater(num: i32) -> String {
+        if num >= 24 {
+            let module_operator_result = num % 24;
+            format!("{}",zero_formater(module_operator_result))
+                       
+        }else{
+            format!("{}",zero_formater(num))
         }
+    }
+}
+pub fn zero_formater(num:i32) -> String {
+    if num.to_string().capacity() == 1 {
+        format!("0{}", num.to_string())
+    }else {
+        num.to_string()
     }
 }
