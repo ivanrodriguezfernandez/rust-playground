22169ea1157abfc3bc49d5ce65d5d4bb6ce82831
ivanrodriguezfernandez <ivanrodriguezfernandez@gmail.com>
2021-02-12 21:56:40 +0100

    update anagram

--- Exercism/rust/anagram/src/lib.rs
+++ Exercism/rust/anagram/src/lib.rs
@@ -1,21 +1,33 @@
-use std::{collections::HashSet, iter::FromIterator};
+use std::collections::HashSet;
 
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &'a [&str]) -> HashSet<&'a str> {
     let mut result = HashSet::new();
+    let word_lower = to_lower_chars(word);
+    let mut word_sorted = word_lower.to_vec();
+    word_sorted.sort_unstable();
+
     for item in possible_anagrams.iter() {
-        
-        if item.to_lowercase() != word.to_lowercase()
-            && ordering(&item.to_lowercase()) == ordering(&word.to_lowercase())
-        {
-            let s: &'a str = &item;
-            result.insert(s);
+        let item_lower = to_lower_chars(item);
+        if item_lower.len() != word_lower.len() || item_lower == word_lower {
+            continue;
+        }
+        let mut item_sorted = item_lower.to_vec();
+        item_sorted.sort_unstable();
+        if item_sorted == word_sorted {
+            result.insert(*item);
         }
     }
     result
 }
 
-pub fn ordering(w: &str) -> String {
-    let mut chars: Vec<char> = w.chars().collect();
-    chars.sort_by(|a, b| b.cmp(a));
-    String::from_iter(&chars)
+fn to_lower_chars(word: &str) -> Vec<char> {
+    word.chars()
+        .map(|c| {
+            if c.is_uppercase() {
+                c.to_lowercase().nth(0).unwrap()
+            } else {
+                c
+            }
+        })
+        .collect::<Vec<char>>()
 }
