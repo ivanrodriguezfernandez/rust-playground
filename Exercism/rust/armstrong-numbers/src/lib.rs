f7ac806b3b9798b33fa5f429302cf700909363d5
ivanrodriguezfernandez <ivanrodriguezfernandez@gmail.com>
2022-04-18 10:46:26 +0200

    Simplify code

--- Exercism/rust/armstrong-numbers/src/lib.rs
+++ Exercism/rust/armstrong-numbers/src/lib.rs
@@ -2,9 +2,12 @@ pub fn is_armstrong_number(num: u32) -> bool {
 
     let mut total: i32 = 0;
 
-    for digit in num.to_string().chars(){
+    let num_str = num.to_string();
+    let num_len = num_str.len() as u32;
 
-        total = total + i32::pow(digit.to_digit(10).unwrap().try_into().unwrap(), num.to_string().chars().count().try_into().unwrap());
+    for digit in num_str.chars()
+    {
+        total = total + i32::pow(digit.to_digit(10).unwrap().try_into().unwrap(), num_len);
     }
     
     total == num.try_into().unwrap()   
